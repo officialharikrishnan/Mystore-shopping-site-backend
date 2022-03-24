31213cb10e9be29ab02b1926867a0e661a9004ba
Harikrishnan <hari@Harikrishnans-MacBook-Air.local>
2022-03-24 20:21:41 +0530

    frontend connected

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
+app.get('/new', function (req, res) {
   res.send('Hello World')
 })
+const port=4000;
 
-app.listen(3000)
+app.listen(port, () =>{console.log(`server started on ${port}`)})
