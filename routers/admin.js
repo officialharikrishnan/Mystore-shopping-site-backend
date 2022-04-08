e0c2da92777ec8e059bf4c92282fe53d1e6e6f39
Harikrishnan <hari@Harikrishnans-MacBook-Air.local>
2022-04-08 22:17:42 +0530

    multi image upload done some updation needed

--- routers/admin.js
+++ routers/admin.js
@@ -5,23 +5,41 @@ const bodyParser = require('body-parser');
 var productHelper=require('../helpers/product-helpers') 
 
 const multer = require("multer")
-const upload=multer();
+var upload=multer();
 app.use(cors());
 // parse application/json
 app.use(bodyParser.json());
+
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        
+      callback(null, './uploads')
+    },
+    filename: function (req, file, callback) {
+      callback(null, file.fieldname + '-' + Date.now())
+    }
+  });
+  var upload = multer({ storage: storage }).array('image1',2);
+  
+
 
 app.get('/admin',(req,res)=>{
     res.send("admin")
 
 })
-app.post('/addproduct', upload.single("image"),(req,res)=>{
+app.post('/addproduct',(req,res)=>{
+  upload(req,res,function(err) {
+    //console.log(req.body);
+    //console.log(req.files);
+    if(err) {
+        return res.end("Error uploading file." + err);
+    }
+    res.end("File is uploaded");
+});
+    const fileInfo = req.files
+    console.log(fileInfo);
     console.log(req.body);
-    console.log(req.file);
-    // productHelper.addProduct(req.body)
+
 
 })
-
-
-
-
 module.exports=app
